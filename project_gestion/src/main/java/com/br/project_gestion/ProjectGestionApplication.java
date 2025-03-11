8d865109aa4f6d84ab58c2ce70e7f871855b316c
Gabriel <gabrielconceicao569@gmail.com>
2025-03-11 09:53:13 -0300

    ProjectGestionApplication

--- project_gestion/src/main/java/com/br/project_gestion/ProjectGestionApplication.java
+++ project_gestion/src/main/java/com/br/project_gestion/ProjectGestionApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.br.project_gestion.infra.repository")
-@EntityScan(basePackages = "com.br.project_gestion.domain.model")
 public class ProjectGestionApplication {
 
 	public static void main(String[] args) {
